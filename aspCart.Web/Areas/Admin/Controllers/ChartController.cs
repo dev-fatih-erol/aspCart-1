0a3a7feb34ce2397894f6a6a36da588e9aabe21e
Fatih Erol <50841052+dev-fatih-erol@users.noreply.github.com>
2020-07-17 21:42:21 +0300

    .

---
+++ aspCart.Web/Areas/Admin/Controllers/ChartController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using aspCart.Core.Interface.Services.Statistics;
+
+namespace aspCart.Web.Areas.Admin.Controllers
+{
+    public class ChartController : AdminController
+    {
+        #region Fields
+
+        private readonly IOrderCountService _orderCountService;
+        private readonly IVisitorCountService _visitorCountService;
+
+        #endregion
+
+        #region Constructor
+
+        public ChartController(
+            IOrderCountService orderCountService,
+            IVisitorCountService visitorCountService)
+        {
+            _orderCountService = orderCountService;
+            _visitorCountService = visitorCountService;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public IActionResult GetVisitorCount()
+        {
+            var results = _visitorCountService.GetAllVisitorCount(7)
+                .OrderBy(x => x.Date);
+
+            return Json(results);
+        }
+
+        public IActionResult GetOrderCount()
+        {
+            var results = _orderCountService.GetAllOrderCount(7)
+                .OrderBy(x => x.Date);
+
+            return Json(results);
+        }
+
+        #endregion
+    }
+}
