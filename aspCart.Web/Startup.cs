2e7119f62220e03fd70aae10a16fee18594e0654
Fatih Erol <50841052+dev-fatih-erol@users.noreply.github.com>
2020-07-17 21:36:40 +0300

    .

--- aspCart.Web/Startup.cs
+++ aspCart.Web/Startup.cs
@@ -1,15 +1,29 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using aspCart.Core.Interface.Services.Catalog;
+using aspCart.Core.Interface.Services.Messages;
+using aspCart.Core.Interface.Services.Sale;
+using aspCart.Core.Interface.Services.Statistics;
+using aspCart.Core.Interface.Services.User;
+using aspCart.Infrastructure;
+using aspCart.Infrastructure.EFModels;
+using aspCart.Infrastructure.EFRepository;
+using aspCart.Infrastructure.Services.Catalog;
+using aspCart.Infrastructure.Services.Messages;
+using aspCart.Infrastructure.Services.Sale;
+using aspCart.Infrastructure.Services.Statistics;
+using aspCart.Infrastructure.Services.User;
+using aspCart.Web.Areas.Admin.Helpers;
 using aspCart.Web.Helpers;
+using aspCart.Web.Models;
 using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Hosting.Internal;
 
 namespace aspCart.Web
 {
@@ -20,7 +34,7 @@ namespace aspCart.Web
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
+        public IConfiguration Configuration { get; set; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -33,6 +47,61 @@ namespace aspCart.Web
             var mapper = mapperConfiguration.CreateMapper();
 
             services.AddSingleton(mapper);
+
+            // Add framework services.
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            // idenity password requirement
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequiredLength = 4;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireLowercase = false;
+            });
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            // configure admin account injectable
+            services.Configure<AdminAccount>(
+                Configuration.GetSection("AdminAccount"));
+
+            services.Configure<UserAccount>(
+                Configuration.GetSection("UserAccount"));
+
+            services.AddDistributedMemoryCache();
+
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(60);
+                options.Cookie.Name = "aspCart";
+            });
+
+            // Add application services.
+            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+            services.AddTransient<IBillingAddressService, BillingAddressService>();
+            services.AddTransient<ICategoryService, CategoryService>();
+            services.AddTransient<IImageManagerService, ImageManagerService>();
+            services.AddTransient<IManufacturerService, ManufacturerService>();
+            services.AddTransient<IOrderService, OrderService>();
+            services.AddTransient<IProductService, ProductService>();
+            services.AddTransient<IReviewService, ReviewService>();
+            services.AddTransient<ISpecificationService, SpecificationService>();
+
+            services.AddTransient<IOrderCountService, OrderCountService>();
+            services.AddTransient<IVisitorCountService, VisitorCountService>();
+
+            services.AddTransient<IContactUsService, ContactUsService>();
+
+
+            services.AddTransient<ViewHelper>();
+            services.AddTransient<DataHelper>();
+
+            // services.AddSingleton<IFileProvider>(HostingEnvironment.ContentRootFileProvider);
 
             services.AddControllersWithViews();
         }
