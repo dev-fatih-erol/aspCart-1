237e2d174854f3b4e8a3e0aa9cfcd68dcfb126f9
Fatih Erol <50841052+dev-fatih-erol@users.noreply.github.com>
2020-08-07 23:43:10 +0300

    .

---
+++ aspCart.Web/ViewComponents/PopulerCategoryViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using aspCart.Core.Interface.Services.Catalog;
+using Microsoft.AspNetCore.Mvc;
+
+namespace aspCart.Web.ViewComponents
+{
+    [ViewComponent(Name = "PopulerCategory")]
+    public class PopulerCategoryViewComponent : ViewComponent
+    {
+        private readonly ICategoryService _categoryService;
+
+        public PopulerCategoryViewComponent(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(
+                _categoryService.GetAllCategoriesWithoutParent().Where(x => x.Published)
+            );
+        }
+    }
+}
